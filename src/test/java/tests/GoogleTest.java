91322b7c7912e7bb8d778209ae709a6f51707f94
Dmitry Rak <dmitryrak11@gmail.com>
2020-10-30 21:02:34 +0300

    Hidden keyword into environment variables

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -9,9 +9,9 @@ public class GoogleTest extends BaseTest {
     @Test(description = "Search in Google")
     @Description("Validation of search functionality in Google")
     @Link("https://instagram.com/dmitryrak11")
-    @Issue("dmitryrak11")
-    @TmsLink("dmitryrak11")
+    @Issue("TMS-123")
+    @TmsLink("22")
     public void googleTest() {
-        steps.searchForKeyWord("teachmeskills");
+        steps.searchForKeyWord(System.getenv("keyword"));
     }
 }
