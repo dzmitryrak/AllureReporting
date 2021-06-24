74a8def6fe4d244040623eacfe57e4c44a4ca6e6
dzmitry.rak <dmitryrak11@gmail.com>
2021-06-24 14:29:15 +0300

    moved to jdk11

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -1,16 +1,14 @@
 package tests.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.extern.log4j.Log4j2;
-import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import steps.GoogleSteps;
-import utils.CapabilitiesGenerator;
 
 @Log4j2
 @Listeners(TestListener.class)
@@ -21,12 +19,8 @@ public class BaseTest {
     
     @BeforeMethod(description = "Opening Chrome Driver")
     public void createDriver(ITestContext context) {
-        try {
-            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
-        } catch (SessionNotCreatedException ex) {
-            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
-            log.fatal(ex.getLocalizedMessage());
-        }
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         steps = new GoogleSteps(driver);
         String variable = "driver";
         System.out.println("Setting driver into context with variable name " + variable);
