5024fdbcfdb79bbe556953be3a55770399bb9616
dzmitry.rak <dmitryrak11@gmail.com>
2022-04-12 18:04:32 +0500

    Changed driver to headless

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -20,6 +20,7 @@ public class BaseTest {
     public void createDriver(ITestContext context) {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 /*        options.addArguments("--remote-debugging-port=9222");
         options.addArguments("no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
